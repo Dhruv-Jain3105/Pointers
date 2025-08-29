58c99f44ccd7edb3483605c3d447c6ce4ce3b201
Dhruv-Jain3105 <dj.dhruvjain3105@gmail.com>
2025-08-29 11:24:36 +0530

    Create ptr_add_sub.cpp

---
+++ ptr_add_sub.cpp
@@ -0,0 +1,22 @@
+/*
+Name:- Dhruv Jain
+PRN:-24070123042
+Batch:-A2
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int a1=100;
+    int b1=50;
+    int *ptr1=&a1;
+    int *ptr2=&b1;
+    int sum=(*ptr1)+(*ptr2);
+    int diff=(*ptr1)-(*ptr2);
+    cout << "SUM : " << sum << endl;
+    cout << "DIFFERENCE: " << diff << endl;
+}
+/*
+Output:-
+SUM : 150
+DIFFERENCE : 50
+*/
