adbd2457e8ff42c2becdf3c50a755a215ae060f0
Dhruv-Jain3105 <dj.dhruvjain3105@gmail.com>
2025-08-29 11:25:08 +0530

    Create reverse_arr.cpp

---
+++ reverse_arr.cpp
@@ -0,0 +1,25 @@
+/*
+Name:- Dhruv Jain
+PRN:-24070123035
+Batch:-A2
+*/
+#include <iostream>
+using namespace std;
+int main() {
+int arr[5]={10,20,30,40,50};
+int *ptr=arr+4;
+cout << "Value of arr: "<< endl;
+for(int i=0;i<5;i++){
+    cout << *ptr << endl;
+    ptr--;
+}
+}
+/*
+Output:-
+Value of arr: 
+50
+40
+30
+20
+10
+*/
