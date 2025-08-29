9703b875b69e0637700e93fc1f54b428f1947d1e
Dhruv-Jain3105 <dj.dhruvjain3105@gmail.com>
2025-08-29 11:24:03 +0530

    Create print_usingptr.cpp

---
+++ print_usingptr.cpp
@@ -0,0 +1,19 @@
+/*
+Name:- Dhruv Jain
+PRN:-24070123042
+Batch:-A2
+*/
+#include <iostream>
+using namespace std;
+int  main(){
+    string str="Hello_world";
+    char *ptr=&str[0];
+    for(int i=0; i<str.length(); i++){
+        cout << *ptr;
+        ptr++;
+    }
+}
+/*
+Output:-
+Hello_world
+*/
